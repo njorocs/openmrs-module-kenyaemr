a1ed528937d73e06278b979cebd7fadd0bb6f87c
ckote <kotecosmas@gmail.com>
2026-03-24 10:28:11 +0300

    `Added new metadata for Transfusion Services`

--- api/src/main/java/org/openmrs/module/kenyaemr/metadata/NCDMetadata.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/metadata/NCDMetadata.java
@@ -32,6 +32,7 @@ public class NCDMetadata extends AbstractMetadataBundle {
 	public static final class _EncounterType {
 		public static final String NCD_INITIAL = "dfcbe5d0-1afb-48a0-8f1e-5e5988b11f15";
 		public static final String NCD_FOLLOWUP = "b402d094-bff3-4b31-b167-82426b4e3e28";
+		public static final String TELEHEATH_CONSULTATION = "d91cd0b0-64dc-4680-b942-0848315a8d91";
 		public static final String NCD_DISCONTINUATION = "c31d1a11-f0a5-4a64-817d-25e5134f2e37";
 	}
 
@@ -60,6 +61,7 @@ public class NCDMetadata extends AbstractMetadataBundle {
 	public void install() {
 		install(encounterType("NCD Enrollment", "Enrolment into NCD", _EncounterType.NCD_INITIAL));
 		install(encounterType("NCD Followup", "NCD followup encounter", _EncounterType.NCD_FOLLOWUP));
+		install(encounterType("Telehealth Consultation", "Telehealth consultation encounter", _EncounterType.TELEHEATH_CONSULTATION));
 		install(encounterType("NCD Discontinuation", "NCD program discontinuation encounter", _EncounterType.NCD_DISCONTINUATION));
 
 		boolean installForms = shouldInstallForms();
@@ -70,7 +72,7 @@ public class NCDMetadata extends AbstractMetadataBundle {
 			install(form("NCD Initial Form", null, _EncounterType.NCD_INITIAL, "1", _Form.NCD_INITIAL_FORM));
 			install(form("NCD Followup Form", null, _EncounterType.NCD_FOLLOWUP, "1", _Form.NCD_FOLLOWUP_FORM));
 			install(form("NCD Discontinuation Form", null, _EncounterType.NCD_DISCONTINUATION, "1", _Form.NCD_DISCONTINUATION_FORM));
-			install(form("Telehealth Consultation Form", null, _EncounterType.NCD_FOLLOWUP, "1", _Form.TELEHEALTH_CONSULTATION_FORM));
+			install(form("Telehealth Consultation Form", null, _EncounterType.TELEHEATH_CONSULTATION, "1", _Form.TELEHEALTH_CONSULTATION_FORM));
 		} else {
 			logger.info("=== NCDMetadata: SKIPPING form installation because shouldInstallForms() returned false ===");
 		}
